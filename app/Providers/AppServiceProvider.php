0724016f6885e05b701f1ed0b84526689eb33e32
Dogi <flamebook@Dogis-MacBook-Pro.fritz.box>
2022-03-11 15:00:30 +0100

    add floating chat

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (true) {
+            //next
             URL::forceScheme('https');
         }
     }
